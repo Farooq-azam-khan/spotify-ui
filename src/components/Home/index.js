795c016b552a91a5ca53b379a41ec678a93cc3db
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-06-25 23:03:37 -0400

    display extra section on large screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import Header from './Header';
 
 const Home = ({ className }) => {
     return (<div className={className}>
-        <Sidebar className="w-2/6 overflow-auto bg-gray-900 h-full flex flex-col" />
+        <Sidebar className="w-1/6 overflow-auto bg-gray-900 h-full flex flex-col" />
 
         <div className="w-4/6 flex flex-col items-start">
             <Header className="w-full flex  justify-between items-start px-3 py-3" />
@@ -14,6 +14,7 @@ const Home = ({ className }) => {
 
             </div>
         </div>
+        <div className="hidden sm:block bg-gray-900 sm:w-1/6 flex flex-col items-start">Magna in esse culpa anim. Et esse nisi in consequat in pariatur quis anim irure consectetur exercitation dolor est culpa. Nulla voluptate velit deserunt magna nulla proident esse culpa cillum culpa.</div>
     </div>
     )
 }
