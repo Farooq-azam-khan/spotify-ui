acf03f5835a8e1b63c17fe4bd66efee03cad3c6c
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2020-06-26 00:09:09 -0400

    added right sidebar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 import Header from './Header';
 
 const Home = ({ className }) => {
-    return (<div className={className}>
+    return (<section className={className}>
         <Sidebar className="w-1/6 overflow-auto bg-gray-900 h-full flex flex-col" />
 
         <div className="w-4/6 flex flex-col items-start">
@@ -14,8 +14,15 @@ const Home = ({ className }) => {
 
             </div>
         </div>
-        <div className="hidden sm:block bg-gray-900 sm:w-1/6 flex flex-col items-start">Magna in esse culpa anim. Et esse nisi in consequat in pariatur quis anim irure consectetur exercitation dolor est culpa. Nulla voluptate velit deserunt magna nulla proident esse culpa cillum culpa.</div>
-    </div>
+        <RightSideBar className="hidden sm:flex w-1/6 flex-col items-center justify-center h-full bg-gray-900 px-5 py-2" />
+    </section>
     )
+}
+
+const RightSideBar = ({ className = "" }) => {
+    return (<section className={className}>
+        <div className="text-center font-semibold md:font-bold text-sm md:text-lg">See what your friends are playing</div>
+        <button className="w-full p-1 md:px-1 md:py-2 border text-white text-xs font-semibold md:font-bold border-gray-100 rounded-full uppercase mt-5">Find Friends</button>
+    </section>)
 }
 export default Home
